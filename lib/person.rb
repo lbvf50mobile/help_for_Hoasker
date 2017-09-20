24582cef350a446d38ac8fafdae1624130f5694f
Dmitry <lbvf50.mobile@gmail.com>
2017-09-20 14:49:29 +0700

    Add test to perosn so it have age, country, and name

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,4 @@
+# Person we going insert in manager
 class Person
   attr_accessor :name
 end
--- spec/source/person_spec.rb
+++ spec/source/person_spec.rb
@@ -7,4 +7,11 @@ describe Person do
     person.name = 'Vasa'
     person.name.should == 'Vasa'
   end
+  it 'had name and age and country' do
+    person = Person.new
+    person.name, person.country, person.age = 'Vasa', 'Rus', 23
+    person.name.should_be('Vasa')
+    person.country.should_be('Rus')
+    person.age.should_be(23)
+  end
 end
